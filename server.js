7f863c3db8725b440996397bb9e9cf39516704e4
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-03-12 09:19:19 -0600

    Add logger morgan

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
+const morgan= require('morgan');
 const app = express();
 
 // API file for interacting with MongoDB
@@ -11,6 +12,7 @@ const app = express();
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false}));
 
+app.use(morgan('dev'));
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
